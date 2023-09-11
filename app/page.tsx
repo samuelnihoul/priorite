3c0dbe245b21d491333222a2f28dd4ec5e4ca631
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-09-11 20:34:48 +0200

    pub

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, onAuthStateChanged, getAuth, signInWithPopup, User } from 'firebase/auth';
